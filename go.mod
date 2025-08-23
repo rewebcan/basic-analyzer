5cb2d689d0f3972b01d048bea99f3312b225eea0
rewebcan <rewebcan@gmail.com>
2025-08-23 15:29:01 +0300

    first optimistic poc using tokenizer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rewebcan/url-fetcher-home24
 
 go 1.24.2
+
+require golang.org/x/net v0.43.0
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	xhtml "golang.org/x/net/html"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+var url = "https://crawler-test.com/mobile/separate_desktop_with_different_h1"
 
 func main() {
-	fmt.Println("Hello World")
+	client := &http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	response, err := client.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	z := xhtml.NewTokenizer(response.Body)
+
+	for {
+		tt := z.Next()
+
+		switch tt {
+		case xhtml.ErrorToken:
+			if z.Err() == io.EOF {
+				return
+			}
+		case xhtml.DoctypeToken:
+			fmt.Println("HTML Found")
+			break
+		case xhtml.StartTagToken, xhtml.SelfClosingTagToken:
+			fmt.Printf("%s", z.Token().Data)
+			break
+		default:
+			continue
+		}
+	}
 }
